35f605355a6c703a390db0eb06868cf05d20d728
spotsccc <chernenko.ivan.k17@gmail.com>
2021-03-20 14:09:15 +0300

    add bundling and minifications

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const path = require('path');
+const TerserPlugin = require("terser-webpack-plugin");
+
+module.exports = {
+    entry: './dist/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    optimization: {
+        minimize: true,
+        minimizer: [new TerserPlugin()],
+    },
+};
