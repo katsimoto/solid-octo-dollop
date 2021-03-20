cf5ea40cc3960ccf205e70cbcf63ecb00e7b257f
spotsccc <chernenko.ivan.k17@gmail.com>
2021-03-20 13:31:50 +0300

    add logging value in console

--- dist/index.js
+++ dist/index.js
@@ -1,13 +1,17 @@
 const form = document.querySelector('.bank-card-form');
 if (form) {
     const button = form.querySelector('button');
-    const formData = new FormData(form);
-    if (button) {
+    const cardMember = form.querySelector('#card-member');
+    const cardNumber = form.querySelector('#card-number');
+    const scv = form.querySelector('#csv');
+    const date = form.querySelector('#date');
+    if (button && scv && cardMember && cardNumber && date) {
         button.addEventListener('click', (e) => {
             e.preventDefault();
-            for (let value of formData.values()) {
-                console.log(value);
-            }
+            console.log(`Номер карты: ${cardNumber.value}`);
+            console.log(`Владелец карты: ${cardMember.value}`);
+            console.log(`Дата: ${date.value}`);
+            console.log(`CSV: ${scv.value}`);
         })
     }
 }
